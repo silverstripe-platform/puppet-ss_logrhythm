60442fbd07c5d166568214d8c1a099df70fd4d24
Brett Tasker <brett.tasker@gmail.com>
2021-12-21 14:25:48 +1300

    Release/1.0.0 (#1)
    * Update Logrhytm to split out agent and client installs with validation
    
    * Add Logrhytm agent port configuration to client
    
    Co-authored-by: Brett Tasker <brett@silverstripe.com>

--- manifests/client.pp
+++ manifests/client.pp
@@ -1,6 +1,10 @@
 # Setup LogRhythm client
 # Client forwards rsyslog logs to agent server
-class ss_logrhythm::client inherits ss_logrhythm {
+class ss_logrhythm::client (
+  Stdlib::Compat::Ip_address  $agent_ip,          # Platform agent server ip to forward syslog messages to
+  Integer                     $agent_port = 514,  # Platform agent server port to forward syslog messages to
+) inherits ss_logrhythm {
+  # Add logryhthm rsyslog configuration
   file { 'logrhythm_syslog.conf':
     path    => '/etc/rsyslog.d/logrhythm.conf',
     content => template('ss_logrhythm/rsyslogd_client.conf.erb'),
